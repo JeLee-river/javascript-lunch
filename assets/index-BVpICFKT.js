590ca13b17d31933038ce39fb11cc6ab46f4e36d
Jeongeun Lee <leeju1009@gmail.com>
2025-03-07 17:13:32 +0900

    Updates

--- assets/index-BVpICFKT.js
+++ assets/index-BVpICFKT.js
@@ -88,12 +88,12 @@ const ADD_BUTTON = {
   content: "추가하기"
 };
 const IMAGE = /* @__PURE__ */ new Map([
-  ["한식", "/category-korean.png"],
-  ["중식", "/category-chinese.png"],
-  ["일식", "/category-japanese.png"],
-  ["양식", "/category-western.png"],
-  ["아시안", "/category-asian.png"],
-  ["기타", "/category-etc.png"]
+  ["한식", "category-korean.png"],
+  ["중식", "category-chinese.png"],
+  ["일식", "category-japanese.png"],
+  ["양식", "category-western.png"],
+  ["아시안", "category-asian.png"],
+  ["기타", "category-etc.png"]
 ]);
 const RESTAURANTS = [
   {
